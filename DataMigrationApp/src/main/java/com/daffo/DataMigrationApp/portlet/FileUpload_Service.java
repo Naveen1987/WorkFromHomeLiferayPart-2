10e482d1eef6c614d6142492ccf559a84c94dffa
unknown <kr.naveen@hotmail.com>
2017-07-01 20:23:36 +0530

    I have invented a new concept to use ddmStructure from other api next

--- DataMigrationApp/src/main/java/com/daffo/DataMigrationApp/portlet/FileUpload_Service.java
+++ DataMigrationApp/src/main/java/com/daffo/DataMigrationApp/portlet/FileUpload_Service.java
@@ -5,9 +5,14 @@ import java.util.List;
 import com.liferay.document.library.kernel.model.DLFolder;
 import com.liferay.document.library.kernel.model.DLFolderConstants;
 import com.liferay.document.library.kernel.service.DLAppServiceUtil;
+import com.liferay.document.library.kernel.service.DLFileEntryLocalServiceUtil;
+import com.liferay.document.library.kernel.service.DLFileEntryServiceUtil;
 import com.liferay.document.library.kernel.service.DLFolderLocalServiceUtil;
+import com.liferay.document.library.kernel.service.persistence.DLFileEntryMetadataUtil;
 import com.liferay.dynamic.data.mapping.model.DDMStructure;
 import com.liferay.dynamic.data.mapping.service.DDMStructureLocalServiceUtil;
+import com.liferay.dynamic.data.mapping.storage.Field;
+import com.liferay.dynamic.data.mapping.storage.Fields;
 import com.liferay.portal.kernel.repository.model.Folder;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 
@@ -34,6 +39,7 @@ public class FileUpload_Service {
 	//Only of Current Site Only
 	public List<Folder> getAllFolder(ThemeDisplay themeDisplay){
 		long repositoryId = themeDisplay.getScopeGroupId();
+		
 		List<Folder> folders=null;
 		 try {
 		      folders = DLAppServiceUtil.getFolders(repositoryId, DLFolderConstants.DEFAULT_PARENT_FOLDER_ID);
@@ -47,13 +53,31 @@ public class FileUpload_Service {
 		 return folders;
 	} 
 	public List<DDMStructure> getAllDocumentType(){
-		List<DDMStructure> structures =DDMStructureLocalServiceUtil.getDDMStructures(0, DDMStructureLocalServiceUtil.getDDMStructuresCount()); 
-		for(DDMStructure dms:structures){
-			System.out.println(dms.getStructureId()+" "+dms.getNameCurrentValue());
-			System.out.println(dms.getFieldNames());
-			
-		}
+		
+		List<DDMStructure> structures =DDMStructureLocalServiceUtil.getStructures();//or//DDMStructureLocalServiceUtil.getDDMStructures(0, DDMStructureLocalServiceUtil.getDDMStructuresCount()); 
+		/*		for(DDMStructure dms:structures){
+					System.out.println(dms.getStructureId()+" "+dms.getNameCurrentValue()+" ");
+					System.out.println(dms.getFieldNames());
+					
+				}*/
 		//System.out.println(structures);
 		return structures;
 	}
+	public List<DDMStructure> getSiteDocumentType(ThemeDisplay themeDisplay){
+		List<DDMStructure> structures =DDMStructureLocalServiceUtil.getStructures(themeDisplay.getScopeGroupId()); 
+		/*		for(DDMStructure dms:structures){
+			System.out.println(dms.getStructureId()+" "+dms.getNameCurrentValue()+" ");
+			System.out.println(dms.getFieldNames());
+			
+		}*/
+		//System.out.println(structures);
+		return structures;		
+	}
+	
+	public void addField(){
+		Fields fields=new Fields();
+		fields.put(new Field("COD", "102"));
+		
+	}
+	
 }
--- DataMigrationApp/src/main/resources/META-INF/resources/view.jsp
+++ DataMigrationApp/src/main/resources/META-INF/resources/view.jsp
@@ -1,10 +1,18 @@
+<%@page import="com.liferay.dynamic.data.mapping.service.DDMStructureLocalServiceUtil"%>
+<%@page import="java.util.Set"%>
+<%@page import="com.liferay.dynamic.data.mapping.model.DDMStructure"%>
+<%@page import="com.liferay.portal.kernel.theme.ThemeDisplay"%>
 <%@page import="com.liferay.portal.kernel.repository.model.Folder"%>
 <%@page import="com.liferay.document.library.kernel.model.DLFolder"%>
 <%@page import="com.daffo.DataMigrationApp.portlet.FileUpload_Service"%>
 <%@page contentType="text/html; charset=UTF-8" language="java" %>
 <%@ include file="/init.jsp" %>
+
+<%FileUpload_Service fus=new FileUpload_Service(); %>
+
 <div class="container-fluid-1280">
-<div class="row">
+
+<%-- <div class="row">
 <div class="col-xs-12">
 <table class=" table table-bordered table-hover">
 <thead>
@@ -17,7 +25,7 @@
 </thead>
 
 <%
-FileUpload_Service fus=new FileUpload_Service();
+
 for(DLFolder fu:fus.getAllDLFolder()){
 	%>
 	<tr>
@@ -30,6 +38,8 @@ for(DLFolder fu:fus.getAllDLFolder()){
 </table>
 </div>
 </div>
+ --%>
+ 
 <div class="row">
 <div class="col-xs-12">
 <table  class=" table table-bordered table-hover">
@@ -56,7 +66,67 @@ for(Folder fu:fus.getAllFolder(themeDisplay)){
 </table>
 </div>
 </div>
-</div>
+
+<%-- <div class="row">
+<div class="col-xs-12">
+<table  class=" table table-bordered table-hover">
+<thead>
+<tr class="bg-primary">
+<td colspan="3">List Of All Document Type</td>
+</tr>
+<tr class="bg-primary">
+<td>Structure ID</td> <td>Document Type</td><td>Document Fields</td>
+</tr>
+</thead>
+
 <%
-fus.getAllDocumentType();
+
+for(DDMStructure fu:fus.getAllDocumentType()){
+	%>
+	<tr>
+	<td><%=fu.getStructureId() %></td><td><%=fu.getNameCurrentValue()%></td><td><%=fu.getFieldNames() %></td>
+	</tr>
+	<%
+}
 %>
+<tbody></tbody>
+</table>
+</div>
+</div> --%>
+
+<div class="row">
+<div class="col-xs-12">
+<table  class=" table table-bordered table-hover">
+<thead>
+<tr class="bg-primary">
+<td colspan="3">List Of Document Type In Sites</td>
+</tr>
+<tr class="bg-primary">
+<td>Structure ID</td> <td>Document Type</td><td>Document Fields</td>
+</tr>
+</thead>
+
+<%
+
+for(DDMStructure fu:fus.getSiteDocumentType(themeDisplay)){
+	%>
+	<tr>
+	<td><%=fu.getStructureId() %></td><td><%=fu.getNameCurrentValue()%></td><td>
+	<% Set<String> fields=	fu.getFieldNames();
+	for(String field:fields){
+		out.println("["+field+"="+fu.getFieldDataType(field)+" "+fu.getFieldRequired(field)+"]");
+	}
+	%>
+	</td>
+	</tr>
+	<%
+}
+%>
+<tbody></tbody>
+</table>
+</div>
+</div>
+</div>
+
+
+
